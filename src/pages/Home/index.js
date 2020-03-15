c3d72f838d19fc7823abeb56c82ec327146867bc
JVictorGalvao <joaogalvao@eng.ci.ufpb.br>
2020-03-15 10:58:29 -0300

    Update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,16 @@ import { Text, View, Button, FlatList, TouchableOpacity } from 'react-native';
 import {Searchbar} from 'react-native-paper';
 import {Avatar} from 'react-native-elements';
 
+function getMoviesFromApiAsync() {
+  return fetch(`https://api.github.com/users/${item.url}`)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson.user;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 export default class Home extends React.PureComponent {
   static navigationOptions = {
       title: 'Home'
@@ -29,7 +39,6 @@ export default class Home extends React.PureComponent {
         console.log(error);
       });
   };
-
   _renderItem = ({item}) => {
       return  (
           <TouchableOpacity onPress={()=>this._onItemPress(item)} style={{flexDirection:'row', padding: 10, alignItems:'center'}}>
@@ -73,96 +82,3 @@ export default class Home extends React.PureComponent {
       )
   }
 }
-
-
-/*export default function Home({navigation}) {
-
-  function navigateToProfile(){
-    navigation.navigate('Profile')
-  }
-  
-  return (
-      
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'flex-start'}}>
-        <Api />
-        <Button title="esse" onPress={navigateToProfile}/>
-        
-      </View>   
-  );
-}
-
-class Api extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: '',
-      isLoading: true,
-    };
-  }
-
-  fetchData = () => {
-    const user = this.state.user;
-    return fetch(`https://api.github.com/search/users?q=${user}`)
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({
-          isLoading: false,
-          dataSource: responseJson.items,
-        });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-  render() {
-    return (
-      <View
-        style={{flex: 1, alignItems: 'center', justifyContent: 'flex-start'}}>
-        <Searchbar
-          placeholder="Enter Github username"
-          autoCapitalize="none"
-          autoCorrect={false}
-          value={this.props.user}
-          onChangeText={user => this.setState({user})}
-          onIconPress={this.fetchData}
-        />
-        <FlatList
-          style={{alignContent: 'stretch', alignSelf: 'stretch'}}
-          data={this.state.dataSource}
-          renderItem={({item}) => (
-            <TouchableOpacity
-              onPress={() => navigateToProfile()} //trocar de pagina
-              style={{
-                flexDirection: 'row',
-                padding: 10,
-                alignItems: 'stretch',
-              }}>
-              <Avatar
-                size="large"
-                rounded
-                source={{
-                  uri: `${item.avatar_url}`,
-                }}
-              />
-              <Text
-                style={{
-                  marginTop: 10,
-                  marginLeft: 10,
-                  fontSize: 20,
-                  fontWeight: 'bold',
-                }}>
-                {item.login}
-              </Text>
-              <Text style={{textAlignVertical: 'auto', textAlign: 'left'}}>
-                {' '}
-                {item.type}
-              </Text>
-            </TouchableOpacity>
-          )}
-          keyExtractor={({id}) => id.toString()}
-        />
-        </View>
-    );
-  }
-}*/
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Text, View, FlatList } from 'react-native';
 import {Avatar} from 'react-native-elements';
 
-
-// import { Container } from './styles';
-
 export default class Profile extends React.PureComponent {
   
   static navigationOptions = {
@@ -18,28 +15,8 @@ export default class Profile extends React.PureComponent {
       user: '',
     }
   }
-/*componentDidMount(){ 
-    return fetch(`${this.props.route.params.item.url}`)
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({
-          isLoading: false,
-          data: responseJson.users,
-        });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-_renderItem = ({item}) => {
-  return  (
-      <Text>
-        {item.url}
-      </Text>
-  )
-}*/
   render(){
-  const { User } = this.props.route.params;
+    const { User } = this.props.route.params;
   return (
     <View style = {{flex: 1, alignItems: 'center'}}>
       <View style = {{padding: 25}}>
